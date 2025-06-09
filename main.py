ed8679d14464fd7ba814d7a4c6b16825a83abbab
Данил <danil.arhishin@mail.ru>
2025-06-09 13:46:11 +0300

    Hello World 1

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from datetime import datetime
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 1
+
+def measure_execution_time(func):
+    def wrapper(*args, **kwargs):
+        t_start = datetime.now()
+        result = func(*args, **kwargs)
+        t_finish = datetime.now()
+        execution_time = t_finish - t_start
+        milliseconds = round(execution_time.microseconds / 1000)
+        print(f"Function completed in "
+              f"{execution_time.seconds}s {milliseconds}ms")
+        return result
+
+    return wrapper
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@measure_execution_time
+def tim_req():
+    response = requests.get('https://google.com')
+    return response
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print(tim_req())
+
+
+# 2
+
+def requires_admin(func):
+    def wrapper(*args, **kwargs):
+        if args[0]["role"] == "admin":
+            return func(*args, **kwargs)
+        else:
+            raise PermissionError()
+
+    return wrapper
+
+
+@requires_admin
+def delete_user(user, username_to_delete):
+    return f"User {username_to_delete} has been deleted by {user['username']}."
+
+
+# Пример юзеров
+
+admin_user = {'username': 'Alice', 'role': 'admin'}
+
+regular_user = {'username': 'Bob', 'role': 'user'}
+
+# Вызовы функции
+
+print(delete_user(admin_user, 'Charlie'))  # Должно отработать
+
+print(delete_user(regular_user, 'Charlie'))  # Должно рейзить PermissionError
