5e110c055b9d5d56eb59ca1e4aebd6431f444da2
Данил <danil.arhishin@mail.ru>
2025-06-21 23:16:03 +0300

    Site in Flask

--- practicum1/app/routes.py
+++ practicum1/app/routes.py
@@ -1,20 +1,37 @@
+import locale
 from flask import render_template, request, redirect, url_for
 from practicum1.app import app
-
+from datetime import datetime
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    locale.setlocale(locale.LC_ALL, 'ru_RU')
+    current_time = datetime.now()
+    return render_template("index.html", current_time=current_time)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    team_members = [
+        {'name': 'Alice', 'role': 'Developer'},
+        {'name': 'Bob', 'role': 'Designer'},
+        {'name': 'Charlie', 'role': 'Project Manager'}
+    ]
+    return render_template("about.html", team_members=team_members)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    info = {
+        'name': 'Служба поддержки',
+        'address': {
+            'street': 'ул. Центральная дом 5',
+            'city': 'Москва',
+            'zip': '12345'
+        }
+    }
+
+    return render_template("contact.html", info=info)
 
 
 @app.route("/submit", methods=["POST", "GET"])
